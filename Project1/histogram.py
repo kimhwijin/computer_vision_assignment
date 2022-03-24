9505d2846490ad62c1de4830d23b37fd2f768987
kimhwijin <rlagnlwls0160@gmail.com>
2022-03-24 20:41:11 +0900

    get histogram with sliding window

--- Project1/histogram.py
+++ Project1/histogram.py
@@ -1,3 +1,5 @@
+import enum
+from operator import mod
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -11,13 +13,53 @@ def get_histogram(src_patches, dst_patches):
         plt.show()
 
 def get_histogram_seperate_channel(src_patches, dst_patches):
+    # n x 9 x 9 x 3
+    # n x 27 x 27 x 3
+
+    window_index = build_2d_window_index(src_patches.shape, dst_patches.shape)
+
+    bins = range(0, 255, 5)
+    # n, bins-1, c
+    src_hist = np.empty((src_patches.shape[0], len(bins) - 1, src_patches.shape[-1]), np.float32)
+    # w, n, bins-1, c
+    dst_hist = np.empty((len(window_index), dst_patches.shape[0], len(bins) - 1, dst_patches.shape[-1]), np.float32)
+
     t_src_patches = np.transpose(src_patches, (0, 3, 1, 2))
-    print(t_src_patches.shape)
-    for src_patch in t_src_patches:
-        print(src_patch.shape)
-        for src_channel in src_patch:
-            print(src_channel.shape)
-            hist, bin_edges = np.histogram(src_channel, bins=range(0, 255, 5))
-            print(hist)
-            plt.hist(hist, bin_edges)
-            plt.show()
+
+    for n, src_patch in enumerate(t_src_patches):
+        for c, src_channel in enumerate(src_patch):
+            src_hist[n,:,c], _ = np.histogram(src_channel, bins=bins)
+
+    t_dst_patches = np.transpose(dst_patches, (0, 3, 1, 2))
+    
+    for n, dst_patch in enumerate(t_dst_patches):
+        for c, dst_channel in enumerate(dst_patch):
+            for w, (h_s, h_e, w_s, w_e) in enumerate(window_index):
+                dst_hist[w, n, :, c], _ = np.histogram(dst_channel[h_s:h_e,w_s:w_e], bins=bins)
+
+    return src_hist, dst_hist
+
+def build_2d_window_index(src_patch_shape, dst_patch_shape):
+    n_windows = (dst_patch_shape[1] - src_patch_shape[1] + 1) * (dst_patch_shape[2] - src_patch_shape[2] + 1)
+    window_index = []
+    #0, 27, 0, 27
+    h_s, h_e, w_s, w_e = 0, src_patch_shape[1], 0, src_patch_shape[2]
+
+    for _ in range(n_windows):
+        window_index.append((h_s, h_e, w_s, w_e))
+
+        if w_e == dst_patch_shape[2]:
+            w_s = 0
+            w_e = src_patch_shape[2]
+            h_s += 1
+            h_e += 1
+        else:
+            w_s += 1
+            w_e += 1
+    
+    return window_index
+        
+
+
+    
+
--- Project1/main.py
+++ Project1/main.py
@@ -2,6 +2,8 @@ from patch_generator import get_patches
 from histogram import *
 
 if __name__ == '__main__':
-    src_patch, dst_patch = get_patches()
-    get_histogram_seperate_channel(src_patch, dst_patch)
+    src_patches, dst_patches = get_patches()
+    src_hist, dst_hist = get_histogram_seperate_channel(src_patches, dst_patches)
+    print(src_hist.shape, dst_hist.shape)
+
     
